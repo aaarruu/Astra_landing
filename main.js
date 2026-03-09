8034724d670306639a441e3571eb2836a909b098
RinataAymaletdinova <rinaymaletdin@mail.ru>
2026-03-09 17:37:39 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import { initCards } from "./js/cards.js"
+import { initMinigame } from "./js/minigame.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+initCards()
+initMinigame()
+
+})
