dc24a94ce3c00e9ef2f970694ec699b20112febb
RinataAymaletdinova <rinaymaletdin@mail.ru>
2026-03-11 20:08:12 +0300

    interactive + mobile

--- main.js
+++
@@ -1,9 +0,0 @@
-import { initCards } from "./js/cards.js"
-import { initMinigame } from "./js/minigame.js"
-
-document.addEventListener("DOMContentLoaded", () => {
-
-initCards()
-initMinigame()
-
-})
