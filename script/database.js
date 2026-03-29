d751facec36799d8650bc0887c234f5a4ffbb643
alenaavtoreeva <125034796+alenaavtoreeva@users.noreply.github.com>
2026-03-29 20:41:07 +0300

    Update database.js

--- script/database.js
+++ script/database.js
@@ -7,8 +7,9 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
         const name = document.getElementById('name').value
         const email = document.getElementById('email').value
         const phone = document.getElementById('phone').value
+		const message = document.getElementById('message').value
 
-        if (name === "" || email === "" || phone === "") {
+        if (name === "" || email === "" || phone === "" || message === "") {
             alert('Заполните все поля!')
             return
         }
@@ -18,7 +19,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
             return
         }
 
-        supabase.from('messages').insert([{ name: name, email: email, phone: phone }])
+        supabase.from('messages').insert([{ name: name, email: email, phone: phone, message: message  }])
             .then(result => {
                 if (result.error) {
                     alert('Ошибка!')
